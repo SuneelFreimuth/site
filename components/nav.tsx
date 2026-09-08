2e99944aa2ede5dbe9aebeef8ee02248dbb1878b
suneel-pi <suneel@2pir.ai>
2026-09-07 20:03:49 -0700

    Poke

--- components/nav.tsx
+++ components/nav.tsx
@@ -21,9 +21,9 @@ function DesktopNav({ links }: {
       </div>
       <nav className='flex justify-end gap-2'>
         {links?.map(link => (
-          <Link key={link.href} href={link.href ?? "/"} className='pointer-events-auto p-2 rounded-md hover:bg-white/10 transition'>
+          <a key={link.href} href={link.href ?? "/"} className='pointer-events-auto p-2 rounded-md hover:bg-white/10 transition'>
             <span>{link.content}</span>
-          </Link>
+          </a>
         ))}
       </nav>
     </div>
